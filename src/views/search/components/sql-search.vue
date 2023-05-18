b2b9eb6c327a8ad9d294207640d8d11456267576
tss <shanshan.tong@timecho.com>
2023-05-18 18:32:18 +0800

    fix: code

--- src/views/search/components/sql-search.vue
+++ src/views/search/components/sql-search.vue
@@ -259,6 +259,7 @@ function handleCommandDown(val: string, index: number) {
     sqlResult.value[index].startQueryTime = dayjs().format('YYYY-MM-DD HH:mm:ss');
     columnList.value.splice(index, 1, []);
     tableData.list.splice(index, 1, {});
+    sqlResult.value[index].sql = code.value[index] || '';
     const { sql = '' } = sqlResult.value[index];
     querySql(props.serverId, { sqls: [sql], timestamp: dayjs(dayjs().format('YYYY-MM-DD HH:mm:ss')).valueOf() })
       .then((res) => {
