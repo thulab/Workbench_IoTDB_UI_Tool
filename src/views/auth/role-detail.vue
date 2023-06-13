c78b70979d7d52acd82b27a49598c080e0e7ffd8
tss <shanshan.tong@timecho.com>
2023-06-13 15:34:14 +0800

    feat: 页面布局修改

--- src/views/auth/role-detail.vue
+++ src/views/auth/role-detail.vue
@@ -1,77 +1,82 @@
 <template>
-  <div class="role-page-container">
-    <div class="role-list-wrapper">
+  <el-container>
+    <el-aside width="240px" class="role-list-wrapper">
       <role-list
         ref="roleListRef"
         @handleSelect="val => currentRole = val"
       />
-    </div>
-    <div class="role-details-wrapper">
-      <h4>权限详情</h4>
-      <el-table :data="tableData" style="width: 100%">
-        <el-table-column label="全选" align="center" width="60" />
-        <el-table-column label="数据库管理" align="center">
-          <el-table-column label="CREATE_DATABASE" align="center" width="180" />
-        </el-table-column>
-        <el-table-column label="时间序列管理" align="center">
-          <el-table-column label="READ" align="center" width="70" />
-          <el-table-column label="WRITE_SCHEMA" align="center" width="140" />
-          <el-table-column label="WRITE_DATA" align="center" width="140" />
-        </el-table-column>
-        <el-table-column label="权限管理" align="center">
-          <el-table-column label="MANAGE_USER" align="center" width="140" />
-          <el-table-column label="MANAGE_ROLE" align="center" width="140" />
-          <el-table-column label="ALTER_PASSWORD" align="center" width="160" />
-        </el-table-column>
-        <el-table-column label="高级功能" align="center">
-          <el-table-column label="TRIGGER" align="center" width="140" />
-          <el-table-column label="CQ" align="center" width="80" />
-        </el-table-column>
-        <el-table-column label="运维命令" align="center">
-          <el-table-column label="MAINTAIN" align="center" width="140" />
-        </el-table-column>
-        <template #empty>
-          <div class="table-empty-wrapper">
-            <img src="@/assets/data-empty.png" alt="" class="data-empty-img">
-            <span class="data-empty-text">无数据</span>
-          </div>
-        </template>
-      </el-table>
-
-      <h4>路径</h4>
-      <el-table :data="tableData" style="width: 100%" tooltip-effect="light">
-        <el-table-column label="路径名称" align="center" width="150" show-overflow-tooltip />
-        <el-table-column label="全选" align="center" width="60" />
-        <el-table-column label="时间序列管理" align="center">
-          <el-table-column label="READ" align="center" min-width="70" />
-          <el-table-column label="WRITE_SCHEMA" align="center" min-width="140" />
-          <el-table-column label="WRITE_DATA" align="center" min-width="140" />
-        </el-table-column>
-        <el-table-column label="操作" align="center">
-          <template #default="{ row }">
-            <el-button>删除{{ row }}</el-button>
+    </el-aside>
+    <el-container class="role-details-wrapper">
+      <el-main class="p-0">
+        <h4>权限详情</h4>
+        <el-table :data="tableData" style="width: 100%">
+          <el-table-column label="全选" align="center" width="60" />
+          <el-table-column label="数据库管理" align="center">
+            <el-table-column label="CREATE_DATABASE" align="center" width="180" />
+          </el-table-column>
+          <el-table-column label="时间序列管理" align="center">
+            <el-table-column label="READ" align="center" width="70" />
+            <el-table-column label="WRITE_SCHEMA" align="center" width="140" />
+            <el-table-column label="WRITE_DATA" align="center" width="140" />
+          </el-table-column>
+          <el-table-column label="权限管理" align="center">
+            <el-table-column label="MANAGE_USER" align="center" width="140" />
+            <el-table-column label="MANAGE_ROLE" align="center" width="140" />
+            <el-table-column label="ALTER_PASSWORD" align="center" width="160" />
+          </el-table-column>
+          <el-table-column label="高级功能" align="center">
+            <el-table-column label="TRIGGER" align="center" width="140" />
+            <el-table-column label="CQ" align="center" width="80" />
+          </el-table-column>
+          <el-table-column label="运维命令" align="center">
+            <el-table-column label="MAINTAIN" align="center" width="140" />
+          </el-table-column>
+          <template #empty>
+            <div class="table-empty-wrapper">
+              <img src="@/assets/data-empty.png" alt="" class="data-empty-img">
+              <span class="data-empty-text">无数据</span>
+            </div>
           </template>
-        </el-table-column>
-        <template #empty>
-          <div class="table-empty-wrapper">
-            <img src="@/assets/data-empty.png" alt="" class="data-empty-img">
-            <span class="data-empty-text">无数据</span>
-          </div>
-        </template>
-      </el-table>
+        </el-table>
 
-      <el-button style="width: 100%;" class="m-t-16" @click="handleAddRow"><i-custom-add class="m-r-4" />添加路径</el-button>
+        <h4>路径</h4>
+        <el-table :data="tableData" style="width: 100%" tooltip-effect="light">
+          <el-table-column label="路径名称" align="center" width="150" show-overflow-tooltip />
+          <el-table-column label="全选" align="center" width="60" />
+          <el-table-column label="时间序列管理" align="center">
+            <el-table-column label="READ" align="center" min-width="70" />
+            <el-table-column label="WRITE_SCHEMA" align="center" min-width="140" />
+            <el-table-column label="WRITE_DATA" align="center" min-width="140" />
+          </el-table-column>
+          <el-table-column label="操作" align="center">
+            <template #default="{ row }">
+              <el-button>删除{{ row }}</el-button>
+            </template>
+          </el-table-column>
+          <template #empty>
+            <div class="table-empty-wrapper">
+              <img src="@/assets/data-empty.png" alt="" class="data-empty-img">
+              <span class="data-empty-text">无数据</span>
+            </div>
+          </template>
+        </el-table>
 
-      <div class="operate-buttons">
-        <el-button>重置</el-button>
-        <el-button type="primary">应用</el-button>
-      </div>
-    </div>
+        <el-button style="width: 100%;" class="m-t-16" @click="handleAddRow"><i-custom-add class="m-r-4" />添加路径</el-button>
+      </el-main>
+
+      <el-footer>
+        <div class="operate-buttons">
+          <el-button>重置</el-button>
+          <el-button type="primary">应用</el-button>
+        </div>
+      </el-footer>
+    </el-container>
 
     <modal-path
       v-model:visible="pathVisible"
+      :path-list="[]"
     />
-  </div>
+  </el-container>
 </template>
 
 <script setup lang="ts">
@@ -113,20 +118,13 @@ watch(
 }
 
 .role-list-wrapper{
-  width: 240px;
-  position: absolute;
-  top: 0;
-  left: 0;
-  height: 100%;
   background-color: #fff;
   border-radius: 6px;
   box-sizing: border-box;
 }
 
 .role-details-wrapper{
-  width: calc(100% - 256px);
-  margin-left: 256px;
-  height: 100%;
+  margin-left: 16px;
   background-color: #fff;
   border-radius: 6px;
   padding: 8px 16px;
