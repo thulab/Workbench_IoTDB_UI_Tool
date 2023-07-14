59c3263373b1c0f25b892ffee6a0e1a0f464bb64
t ss <shanshan.tong@timecho.com>
2023-07-14 15:12:39 +0800

    feat: 升级elementui

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,11 @@
 <script setup lang="ts">
 import { ElConfigProvider } from 'element-plus';
-import zhCn from 'element-plus/lib/locale/lang/zh-cn';
-import en from 'element-plus/lib/locale/lang/en';
-import de from 'element-plus/lib/locale/lang/de';
+import zhCn from 'element-plus/es/locale/lang/zh-cn';
 import useAppStore from '@/stores/app';
-import { useI18n } from 'vue-i18n';
-import { LOCALE } from './constants';
 
 const appStore = useAppStore();
-const { locale: language } = useI18n();
 zhCn.el.pagination.goto = '跳至';
-const locale = computed(() => {
-  switch (language.value) {
-    case LOCALE.ZH_CN:
-      return zhCn;
-    case LOCALE.EN:
-      return en;
-    case LOCALE.DE:
-      return de;
-    default:
-      return zhCn;
-  }
-});
+const locale = computed(() => zhCn);
 
 </script>
 
