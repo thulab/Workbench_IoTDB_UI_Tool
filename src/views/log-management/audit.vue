f66d5c220a9652b5072ce9c53321ec2bfda5fcea
t ss <shanshan.tong@timecho.com>
2023-07-17 14:50:19 +0800

    fix: 审计日志搜索用户交互修改

--- src/views/log-management/audit.vue
+++ src/views/log-management/audit.vue
@@ -4,9 +4,11 @@
       <div class="search-form-wrapper">
         <el-form :model="searchFormData" ref="searchFormRef" label-position="left" size="default" inline>
           <base-form-item label="操作用户：" prop="username">
-            <el-select v-model="searchFormData.username">
-              <el-option v-for="item in userList" :key="item.name" :label="item.name" :value="item.name" />
-            </el-select>
+            <el-input v-model="searchFormData.username" placeholder="请输入用户名称" style="width: 172px;">
+              <template #prefix>
+                <i-custom-search-icon class="remote-select-search-icon" />
+              </template>
+            </el-input>
           </base-form-item>
           <base-form-item label="IP来源：" prop="address">
             <el-input v-model="searchFormData.address" placeholder="请输入 IP 来源" style="width: 172px;">
@@ -104,16 +106,13 @@
 <script setup lang="ts">
 import type { FormInstance } from 'element-plus';
 import { cloneDeep } from 'lodash-es';
-import { useUserStore } from '@/stores';
-import { LogApi, AuthApi } from '@/api';
+import { LogApi } from '@/api';
 import {
   getStartAndEnd, today, formatDate, getOneInterval, getOneIntervalNow,
 } from '@/utils/date';
 import ICustomCalender from '~icons/custom/calender.svg';
 import OverflowClick from './components/overflow-click.vue';
 
-const userStore = useUserStore();
-const userName = computed(() => userStore.userInfo.name || 'root');
 const { maxTableHeight } = useTableHeight(340);
 const searchFormRef = ref<FormInstance>();
 const searchFormData = reactive({
@@ -147,7 +146,6 @@ const totalCount = ref(0);
 const dialogVisible = ref(false);
 const editDetail = ref('');
 
-const { requestFn: getUserList, data: userList } = useRequest(AuthApi.getUserList);
 const { requestFn: getAuditLogList, data: tableData, loading } = useRequest(LogApi.getAuditLogList, {
   initData: {
     totalCount: 0,
@@ -155,10 +153,6 @@ const { requestFn: getAuditLogList, data: tableData, loading } = useRequest(LogA
     list: [],
   },
 });
-
-function getUsers() {
-  getUserList();
-}
 
 function getListData() {
   getAuditLogList({
@@ -176,7 +170,6 @@ function getListData() {
 function handleReset() {
   searchFormRef.value?.resetFields();
   searchFormData.time = getStartAndEnd(0);
-  searchFormData.username = userName.value;
 }
 
 // 查询
@@ -204,9 +197,7 @@ function handleView(row: Log.AuditData) {
 }
 
 onMounted(() => {
-  getUsers();
   handleReset();
-  searchFormData.username = userName.value;
   handleSearch();
 });
 </script>
