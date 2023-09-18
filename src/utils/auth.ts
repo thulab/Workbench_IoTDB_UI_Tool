6070aaf0bb5b6ade4369055d1eda8d926c4bb9df
t ss <shanshan.tong@timecho.com>
2023-09-18 18:31:39 +0800

    feat: 测点权限父路径

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -16,4 +16,15 @@ export const getPathAuthList = (path: string, dataPrivilegeLMap: Array<{ path: s
   return getPathAuthList(path, dataPrivilegeLMap, level + 1);
 };
 
+export const getParentPathAuthList = (path: string, dataPrivilegeLMap: Array<{ path: string, privileges: string[] }> | null | undefined, level: number = 0): string[] => {
+  if (!dataPrivilegeLMap) return [];
+  const pathData = dataPrivilegeLMap.find((item) => level === 0 && (item.path === path || item.path === `${path}.**`));
+  if (pathData) return pathData.privileges;
+  if (path === 'root') return [];
+  path = path.substring(0, path.lastIndexOf('.'));
+  const pathVagueData = dataPrivilegeLMap.find((item) => item.path === `${path}.**`);
+  if (pathVagueData) return pathVagueData.privileges;
+  return getParentPathAuthList(path, dataPrivilegeLMap, level + 1);
+};
+
 export default { getPathAuthList };
