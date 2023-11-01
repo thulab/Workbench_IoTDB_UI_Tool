bc34d2e420a4aade3bd99ae5a936458b2003b30a
t ss <shanshan.tong@timecho.com>
2023-11-01 11:18:43 +0800

    fix: 插件参数名称校验

--- src/views/data-sync/components/modal-sync.vue
+++ src/views/data-sync/components/modal-sync.vue
@@ -542,11 +542,11 @@ const handleConfirm = () => {
           ElMessage.error('历史数据与实时数据状态不能同时为关，请修改后重新操作');
           return;
         }
-        if (formData.value.processorPluginType === 'custom' && formData.value.processorPluginParam.toLocaleLowerCase().includes('processor')) {
+        if (formData.value.processorPluginType === 'custom' && formData.value.processorPluginParam.toLocaleLowerCase().includes("'processor'")) {
           ElMessage.error("插件参数中不能包含 'processor'");
           return;
         }
-        if (formData.value.connectorPluginType === 'custom' && formData.value.connectorPluginParam.toLocaleLowerCase().includes('connector')) {
+        if (formData.value.connectorPluginType === 'custom' && formData.value.connectorPluginParam.toLocaleLowerCase().includes("'connector'")) {
           ElMessage.error("插件参数中不能包含 'connector'");
           return;
         }
