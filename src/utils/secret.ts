8d0fa98484effa5266030045599e48589cf1f3ef
t ss <shanshan.tong@timecho.com>
2024-01-11 18:38:41 +0800

    feat: 密钥

--- src/utils/secret.ts
+++ src/utils/secret.ts
@@ -1,14 +1,14 @@
 import {
-  AES, mode, pad, enc,
+  AES, mode, pad, enc, MD5,
 } from 'crypto-js';
 
 export const encodeAES = (data?: string, key = 'a=cd;fg.ijklmnop8rstu5wx*z12==56') => {
   if (!data) return '';
-  const encrypted = AES.encrypt(data, enc.Utf8.parse(key), {
+  const encrypted = AES.encrypt(data, MD5(enc.Utf8.parse(key)), {
     mode: mode.ECB,
     pad: pad.Pkcs7,
   });
   // 这里的encrypted不是字符串，而是一个CipherParams对象
-  return encrypted.ciphertext.toString(enc.Base64);
+  return encrypted.toString();
 };
 export default { encodeAES };
