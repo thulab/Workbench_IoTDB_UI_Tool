f078f83d103e2900ae14ceadb5a62628e2d127ec
t ss <shanshan.tong@timecho.com>
2024-03-06 17:33:28 +0800

    feat: 系统、激活信息弹框样式

--- src/components/modal-system.vue
+++ src/components/modal-system.vue
@@ -2,41 +2,23 @@
   <el-dialog
     :title="t('auth.systemInfo')"
     v-model="dialogVisible"
-    width="480px"
+    width="265px"
     align-center
     :close-on-click-modal="false"
     id="system-modal"
+    class="system-dialog"
   >
-    <ul>
-      <li class="active-info-item">
-        <span class="active-info-label">{{ t('dashboard.activationExpirationTime') }}</span>
-        <span class="active-info-content"></span>
-      </li>
-      <li class="active-info-item">
-        <span class="active-info-label">{{ t('dashboard.activeDataNodeNum') }}</span>
-        <span class="active-info-content"></span>
-      </li>
-      <li class="active-info-item">
-        <span class="active-info-label">{{ t('dashboard.activeAlNodeNum') }}</span>
-        <span class="active-info-content"></span>
-      </li>
-      <li class="active-info-item">
-        <span class="active-info-label">{{ t('dashboard.activeCpuNum') }}</span>
-        <span class="active-info-content"></span>
-      </li>
-      <li class="active-info-item">
-        <span class="active-info-label">{{ t('dashboard.activeDeviceNum') }}</span>
-        <span class="active-info-content"></span>
-      </li>
-      <li class="active-info-item">
-        <span class="active-info-label">{{ t('dashboard.activeMeasurementNum') }}</span>
-        <span class="active-info-content"></span>
-      </li>
-    </ul>
+    <div class="system-info-box">
+      <img src="@/assets/timecho-logo.svg" alt="" class="system-logo">
+      <p class="system-title">{{ t('auth.systemTitle') }}</p>
+      <p class="system-version">{{ t('auth.versionTitle', { version: appVersion }) }}</p>
+      <p class="system-detail">{{ t('auth.systemDetail') }}</p>
+    </div>
   </el-dialog>
 </template>
 
 <script lang="ts" setup>
+import useAppStore from '@/stores/app';
 
 const props = defineProps<{
   visible: boolean;
@@ -47,15 +29,50 @@ const emit = defineEmits<{
 }>();
 
 const { t } = useI18n();
+const appStore = useAppStore();
 const dialogVisible = useVModel(props, 'visible', emit);
-
-watch(
-  () => props.visible,
-  (newVal) => {
-    if (newVal) {
-      console.log(newVal);
-    }
-  },
-);
+const appVersion = computed(() => appStore.AppVersion);
 
 </script>
+
+<style lang="scss">
+.system-dialog{
+  border-radius: 4px !important;
+}
+</style>
+
+<style lang="scss" scoped>
+.system-info-box{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  .system-logo{
+    width: 120px;
+    height: 32px;
+  }
+
+  .system-title{
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 21px;
+    color: #424561;
+    margin: 12px 0 4px;
+  }
+
+  .system-version {
+    font-size: 12px;
+    font-weight: 300;
+    line-height: 18px;
+    color: #656A85;
+  }
+
+  .system-detail {
+    font-size: 12px;
+    font-weight: 400;
+    line-height: 18px;
+    color: #656A85;
+    margin: 24px 0 0;
+  }
+}
+</style>
--- src/views/dashboard/components/modal-active.vue
+++ src/views/dashboard/components/modal-active.vue
@@ -2,35 +2,36 @@
   <el-dialog
     :title="t('dashboard.activeDetail')"
     v-model="dialogVisible"
-    width="480px"
+    width="370px"
     align-center
     :close-on-click-modal="false"
     id="active-modal"
+    class="active-modal"
   >
     <ul>
       <li class="active-info-item">
-        <span class="active-info-label">{{ t('dashboard.activationExpirationTime') }}</span>
+        <span class="active-info-label">{{ t('dashboard.activationExpirationTime') }}:</span>
         <span class="active-info-content"></span>
       </li>
       <li class="active-info-item">
-        <span class="active-info-label">{{ t('dashboard.activeDataNodeNum') }}</span>
+        <span class="active-info-label">{{ t('dashboard.activeDataNodeNum') }}:</span>
         <span class="active-info-content"></span>
       </li>
       <li class="active-info-item">
-        <span class="active-info-label">{{ t('dashboard.activeAlNodeNum') }}</span>
+        <span class="active-info-label">{{ t('dashboard.activeAlNodeNum') }}:</span>
+        <span class="active-info-content">-</span>
+      </li>
+      <li class="active-info-item">
+        <span class="active-info-label">{{ t('dashboard.activeCpuNum') }}:</span>
         <span class="active-info-content"></span>
       </li>
       <li class="active-info-item">
-        <span class="active-info-label">{{ t('dashboard.activeCpuNum') }}</span>
+        <span class="active-info-label">{{ t('dashboard.activeDeviceNum') }}:</span>
         <span class="active-info-content"></span>
       </li>
       <li class="active-info-item">
-        <span class="active-info-label">{{ t('dashboard.activeDeviceNum') }}</span>
-        <span class="active-info-content"></span>
-      </li>
-      <li class="active-info-item">
-        <span class="active-info-label">{{ t('dashboard.activeMeasurementNum') }}</span>
-        <span class="active-info-content"></span>
+        <span class="active-info-label">{{ t('dashboard.activeMeasurementNum') }}:</span>
+        <span class="active-info-content">50000/25000</span>
       </li>
     </ul>
   </el-dialog>
@@ -59,3 +60,35 @@ watch(
 );
 
 </script>
+
+<style lang="scss">
+.active-modal{
+  border-radius: 4px !important;
+}
+</style>
+
+<style lang="scss" scoped>
+.active-info-item{
+  display: flex;
+  min-height: 32px;
+  align-items: center;
+
+  .active-info-label{
+    flex: 60%;
+    text-align: right;
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 21px;
+    color: #424561;
+  }
+
+  .active-info-content{
+    font-size: 12px;
+    font-weight: 300;
+    line-height: 18px;
+    color: #656A85;
+    flex: 40%;
+    text-align: center;
+  }
+}
+</style>
